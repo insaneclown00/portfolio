019f82edc9a09eb15a381e0e1236fcec967336ac
Optimus Prime <optimusprime596@ymail.com>
2020-03-29 20:41:34 +0600

    change folder name

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'portfolio.urls'
+ROOT_URLCONF = 'my_portfolio.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'portfolio.wsgi.application'
+WSGI_APPLICATION = 'my_portfolio.wsgi.application'
 
 
 # Database
